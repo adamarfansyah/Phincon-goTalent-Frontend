18f3222b5ab26a6271e5d6ee4d52f969c7d99577
adamarfansyah <adam.arfansyah@phincon.com>
2023-11-25 10:13:49 +0700

    frontend

---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import formattedDate from '@utils/formattedDate';
+
+import classes from './style.module.scss';
+
+const Cards = ({ course, id }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={!course.is_close ? classes.card : `${classes.card} ${classes.disabled}`}>
+      <img src={course.image} alt={course?.title} />
+      <div className={classes.date}>{formattedDate(course?.start_date)}</div>
+      <div className={classes.cardDesc}>
+        <div className={classes.desc}>
+          <div className={classes.title}>{course?.title}</div>
+          <div className={classes.description}>{course?.description}</div>
+          <div>{course?.courseInstructor?.fullName}</div>
+          <div>Rp.{course?.price?.toLocaleString()}</div>
+          <div className={classes.participate}>Max Participants {course?.max_participants}</div>
+        </div>
+        {course?.is_close ? null : (
+          <div className={classes.detail} onClick={() => navigate(`/courses/${id}`)}>
+            Detail
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Cards.propTypes = {
+  course: PropTypes.object,
+  id: PropTypes.number,
+};
+
+export default Cards;
